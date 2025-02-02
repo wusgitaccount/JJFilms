cf90d53bbbfb3fc841710fa8ffe04562ca6d213a
Sophia Wu <sophiawu@Sophias-MacBook-Air.local>
2025-02-02 17:02:45 +1300

    added temp filler music

--- script.js
+++ script.js
@@ -1,3 +1,9 @@
 function changevid(buttonlink) {
     document.getElementById('change').src = buttonlink;
 }
+
+document.getElementById("startOrStopImg").onclick = function() {
+    var audio = document.getElementById("audio");
+    if (audio.paused) audio.play();
+    else audio.pause();
+};
