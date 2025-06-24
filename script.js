bc4dad24fc9efee92ea841b03a595be8d65faea3
Sophia Wu <sophiawu@Sophias-MacBook-Air.local>
2025-06-24 14:57:06 +0200

    added missing projects

--- script.js
+++ script.js
@@ -7,3 +7,11 @@ document.getElementById("startOrStopImg").onclick = function() {
     if (audio.paused) audio.play();
     else audio.pause();
 };
+
+
+$(function() {
+   $("body").mousewheel(function(evt, chg) {
+      this.scrollLeft -= (chg * 50); //need a value to speed up the change
+      evt.preventDefault();
+   });
+});
